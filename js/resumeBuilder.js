08169d78eb018ef619017c8faa52af0cc1dbdfe1
John Ryan <johnryan1982@gmail.com>
2018-01-11 01:02:31 +0000

    Create Resume.bio object. Closes #1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,25 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+/// namespace
+const Resume = window.Resume || {};
+
+/// biography at a glance
+Resume.bio = {
+  name: "John Ryan",
+  role: "Front-End Developer",
+  contacts: {
+    mobile: "+44712345678",
+    email: "hello@john-ryan.info",
+    github: "johnryan1982",
+    location: "SW London, UK"
+  },
+
+  welcomeMessage: "A software engineer with over 10 years worth of professional experience in developing a variety of front-end solutions for clients ranging from small startups through to established multi-nationals. These solutions have included simple websites through to large web applications. Specialties include JavaScript, CSS, HTML, Node.js, Dart, PHP, open-source and web performance.",
+
+  skills: [
+    "JavaScript - vanilla JS, ECMAScript 6, Node.js along with popular JS frameworks & libraries"
+  ],
+
+  biopic: "http://gravatar.com/avatar/186b65f0404c63825b28710da670b293?s=220",
+
+  display: function () {
+    console.log('Resume.bio()');
+};
