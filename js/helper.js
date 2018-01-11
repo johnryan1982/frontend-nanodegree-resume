1b49462c7fd82acddd5f54f588452906136fb2be
John Ryan <johnryan1982@gmail.com>
2018-01-11 14:27:06 +0000

    Add HTML markup strings to Resume namespace

--- js/helper.js
+++ js/helper.js
@@ -7,57 +7,60 @@ Don't worry, you'll learn what's going on in this file throughout the course. Yo
 Cameron Pittman
 */
 
+/// namespace
+Resume = window.Resume || {};
 
 /*
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
 */
-var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+Resume.markup = {
+  HTMLheaderName: '<h1 id="name">%data%</h1>',
+  HTMLheaderRole: '<span>%data%</span><hr>',
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+  HTMLcontactGeneric: '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>',
+  HTMLmobile: '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>',
+  HTMLemail: '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>',
+  HTMLtwitter: '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>',
+  HTMLgithub: '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>',
+  HTMLblog: '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>',
+  HTMLlocation: '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>',
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+  HTMLbioPic: '<img src="%data%" class="biopic">',
+  HTMLwelcomeMsg: '<span class="welcome-message">%data%</span>',
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+  HTMLskillsStart: '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>',
+  HTMLskills: '<li class="flex-item"><span class="white-text">%data%</span></li>',
 
-var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
-var HTMLworkDates = '<div class="date-text">%data%</div>';
-var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+  HTMLworkStart: '<div class="work-entry"></div>',
+  HTMLworkEmployer: '<a href="#">%data%',
+  HTMLworkTitle: ' - %data%</a>',
+  HTMLworkDates: '<div class="date-text">%data%</div>',
+  HTMLworkLocation: '<div class="location-text">%data%</div>',
+  HTMLworkDescription: '<p><br>%data%</p>',
 
-var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
-var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+  HTMLprojectStart: '<div class="project-entry"></div>',
+  HTMLprojectTitle: '<a href="#">%data%</a>',
+  HTMLprojectDates: '<div class="date-text">%data%</div>',
+  HTMLprojectDescription: '<p><br>%data%</p>',
+  HTMLprojectImage: '<img src="%data%">',
 
-var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
-var HTMLschoolDates = '<div class="date-text">%data%</div>';
-var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+  HTMLschoolStart: '<div class="education-entry"></div>',
+  HTMLschoolName: '<a href="#">%data%',
+  HTMLschoolDegree: ' -- %data%</a>',
+  HTMLschoolDates: '<div class="date-text">%data%</div>',
+  HTMLschoolLocation: '<div class="location-text">%data%</div>',
+  HTMLschoolMajor: '<em><br>Major: %data%</em>',
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+  HTMLonlineClasses: '<h3>Online Classes</h3>',
+  HTMLonlineTitle: '<a href="#">%data%',
+  HTMLonlineSchool: ' - %data%</a>',
+  HTMLonlineDates: '<div class="date-text">%data%</div>',
+  HTMLonlineURL: '<br><a href="#">%data%</a>',
 
-var internationalizeButton = '<button>Internationalize</button>';
-var googleMap = '<div id="map"></div>';
-
+  HTMLinternationalizeButton: '<button>Internationalize</button>',
+  HTMLgoogleMap: '<div id="map"></div>'
+};
 
 /*
 The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
@@ -154,17 +157,19 @@ function initializeMap() {
   about a single location.
   */
   function createMapMarker(placeData) {
+    /// cache location data
+    const loc = placeData.geometry.location;
 
     // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
+    var lat = loc.lat();  // latitude from the place service
+    var lon = loc.lng();  // longitude from the place service
     var name = placeData.formatted_address;   // name of the place from the place service
     var bounds = window.mapBounds;            // current boundaries of the map window
 
     // marker is an object with additional data about the pin for a single location
     var marker = new google.maps.Marker({
       map: map,
-      position: placeData.geometry.location,
+      position: loc,
       title: name
     });
 
